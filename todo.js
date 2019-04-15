e4286889b1cf27a5c32114c6374a9c7ffe099af8
Mark Lassoff <marklassoff@Marks-MacBook-Pro.local>
2019-04-15 15:25:16 -0400

    Working Model/ Testing Complete

--- todo.js
+++ todo.js
@@ -1,7 +1,8 @@
 const btnSave = document.getElementById("btnSave");
 const toDoItem = document.getElementById("toDoItem");
+const itemsDisplay = document.getElementById("itemsDisplay");
 
-getItems();
+displayItems(getItems());
 
 btnSave.onclick = function() {
   const toDoText = toDoItem.value;
@@ -11,15 +12,52 @@ btnSave.onclick = function() {
 function storeItem(item){
   let items = getItems();
   if(items != undefined && items != ""){
-    items = `${items} , ${item};`
+    items = `${items} , ${item}`;
   } else {
     items = item;
   }
+  saveToDos(items);
+}
+
+function saveToDos(items){
   Lockr.set('Items', items);
+  displayItems(items);
+  clearEntry();
+}
+
+function clearEntry()
+{
+  toDoItem.value = "";
 }
 
 function getItems(){
   const todos = Lockr.get('Items');
-  console.log(todos);
   return todos;
+}
+
+function displayItems(items){
+  if(items != undefined && items != ""){
+    const itemArray = items.split(',');
+    let out = '<ul class="list">';
+    for(var x= 0; x < itemArray.length; x++){
+      out += ' <li class="list-item list-item--longdivider"><div class="list-item__center list-item--longdivider__center">';
+      out += "" + itemArray[x] + "</div>";
+      out += '<div class="list-item__right" onclick="deleteItem(' + x + ');"><i class="zmdi zmdi-delete"></i></div></li>';
+    }
+    out += "</ul>"
+    itemsDisplay.innerHTML = out;
+  }
+}
+
+function deleteItem(itemToDelete){
+  let items = getItems();
+  let itemArray = items.split(',');
+  let newItems = new Array();
+  for(var x = 0; x < itemArray.length; x++){
+    if(x != itemToDelete){
+      newItems.push(itemArray[x]);
+    }
+  }
+  displayItems(newItems.join());
+  saveToDos(newItems.join());
 }
